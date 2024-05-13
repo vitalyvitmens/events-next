1f0e10aae4d17fc027d1d3345003735374c35360
vitalyvitmens <otm_@tut.by>
2024-05-13 15:18:20 +0300

    feat: add get data from db prisma

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma, User } from '../../server/db'
 
-type Data = {
-  name: string;
-};
+type Data = User[]
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const users = await prisma.user.findMany()
+
+  res.status(200).json(users)
 }
