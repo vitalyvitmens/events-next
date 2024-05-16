4b3ebf54ec253f5a08f9c23648aa2c540206a0e4
vitalyvitmens <otm_@tut.by>
2024-05-16 13:43:57 +0300

    chore(src/pages/index.tsx): add leave event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { EventCard } from '@/entities/event'
 import { JoinEventButton } from '@/features/join-event'
+import { LeaveEventButtonProps } from '@/features/leave-event'
 import { trpc } from '@/shared/api'
 
 export default function Home() {
   const { data, refetch } = trpc.event.findMany.useQuery()
+
   return (
     <ul>
       {data?.map((event) => (
@@ -11,8 +13,10 @@ export default function Home() {
           <EventCard
             {...event}
             action={
-              !event.isJoined && (
+              !event.isJoined ? (
                 <JoinEventButton eventId={event.id} onSuccess={refetch} />
+              ) : (
+                <LeaveEventButtonProps eventId={event.id} onSuccess={refetch} />
               )
             }
           />
