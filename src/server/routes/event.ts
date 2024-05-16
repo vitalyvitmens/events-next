65284bde25787ed1f43ef07828dfb6340247bec6
vitalyvitmens <otm_@tut.by>
2024-05-16 13:38:35 +0300

    feat(src/server/routes/event.ts): add update & leave procedure

--- src/server/routes/event.ts
+++ src/server/routes/event.ts
@@ -1,6 +1,10 @@
 import { isAuth, procedure, router } from '../trpc'
 import { prisma } from '../db'
-import { CreateEventSchema, JoinEventSchema } from '@/shared/api/schema'
+import {
+  CreateEventSchema,
+  JoinEventSchema,
+  UpdateEventSchema,
+} from '@/shared/api'
 import { z } from 'zod'
 
 export const eventRouter = router({
@@ -28,6 +32,7 @@ export const eventRouter = router({
       return prisma.event.findUnique({
         where: input,
         select: {
+          authorId: true,
           title: true,
           description: true,
           date: true,
@@ -62,6 +67,36 @@ export const eventRouter = router({
         data: {
           userId: user.id,
           eventId: input.id,
+        },
+      })
+    }),
+
+  update: procedure
+    .input(UpdateEventSchema)
+    .use(isAuth)
+    .mutation(({ input }) => {
+      return prisma.event.update({
+        data: {
+          title: input.title,
+          description: input.description,
+          date: input.date,
+        },
+        where: {
+          id: input.id,
+        },
+      })
+    }),
+
+  leave: procedure
+    .input(JoinEventSchema)
+    .use(isAuth)
+    .mutation(({ input, ctx: { user } }) => {
+      return prisma.participation.delete({
+        where: {
+          userId_eventId: {
+            eventId: input.id,
+            userId: user.id,
+          },
         },
       })
     }),
