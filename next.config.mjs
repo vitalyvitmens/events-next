a9d5825d01b175c77319f8b526c612a05083ee8d
vitalyvitmens <otm_@tut.by>
2024-07-12 14:44:10 +0300

    fix: fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  exportPathMap: async function ({ dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/404': { page: '/404' },
+      '/signup': { page: '/signup' },
+      '/events/create': { page: '/events/create' },
+      '/events/[id]': { page: '/events/[id]' },
+      '/events/[id]/edit': { page: '/events/[id]/edit' },
+      // добавь другие страницы, если нужно
+    }
+  },
 }
-
-export default nextConfig
